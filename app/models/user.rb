63c8a8e61bde0421347e0a7f0f52de0925c84643
sebastian4260 <br7cervantes@gmail.com>
2021-01-14 19:36:46 -0500

    Add associations and validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :liked_tweets, through: :likes, source: :tweet
   has_one_attached :avatar
 
+  validates :name, :username, presence: true
+  validates :username, uniqueness: true
+  enum role: {member: 0, admin: 1}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
