290c6d6334606ebd1413560d6d57b7cf92b0f07d
sebastian4260 <br7cervantes@gmail.com>
2021-01-16 11:08:07 -0500

    Add route to create a repply

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resources :tweets, only: %i[create edit update show destroy]
 
+  resources :tweets, only: %i[create edit update show destroy] do
+    resources :reply_tweets, only: %i[create]
+  end
+
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
   resources :users, only: %i[show]
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
