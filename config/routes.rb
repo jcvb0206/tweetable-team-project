b07a60d07f73f3f1cfe5b9b0a6af45e40ae1d3b9
sebastian4260 <br7cervantes@gmail.com>
2021-01-15 22:24:39 -0500

    Add route to show users

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
+  resources :users, only: %i[show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
