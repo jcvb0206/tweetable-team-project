cc971b25ce052451dd8118250e6e5771fedff88f
richardyantas5 <richadyantas5@gmail.com>
2021-01-15 22:36:33 -0500

    increment and decrement counter is added

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,20 @@
 class Like < ApplicationRecord
+  
   belongs_to :user
-  belongs_to :tweet
+  belongs_to :tweet, counter_cache: true
   validates :tweet_id, uniqueness: { scope: :user_id }
+
+  after_create :increment_counter_likes
+  after_destroy :decrement_counter_likes
+
+  def increment_counter_likes
+    tweet.increment(:likes_count)
+    tweet.save
+  end
+
+  def decrement_counter_likes
+    tweet.decrement(:likes_count)
+    tweet.save
+  end
+
 end
